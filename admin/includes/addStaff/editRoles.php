9788a1e59eeff43462121842bf82085ecf4a925b
amiel0606 <lapid.167399@dasmarinas.sti.edu.ph>
2024-09-10 00:56:10 +0800

    edited roles

--- admin/includes/addStaff/editRoles.php
+++ admin/includes/addStaff/editRoles.php
@@ -29,41 +29,33 @@ if (isset($_POST['submit'])) {
             'removeAdmin' => $_POST['removeAdmin'] ?? 0,
         );
 
-        // Insert the permissions into the role_permissions table
         foreach ($permissions as $permissionName => $permissionValue) {
             if ($permissionValue == 'on') {
-                // Get the permission ID from the permissions table
                 $query = "SELECT id FROM tbl_permissions WHERE name = ?";
                 $stmt = $conn->prepare($query);
                 $stmt->bind_param("s", $permissionName);
                 $stmt->execute();
                 $result = $stmt->get_result();
         
-                if ($result) { // Check if $result is not null
+                if ($result) {
                     $permissionId = $result->fetch_assoc()['id'];
                     $stmt->close();
-        
-                    // Insert the role-permission association into the role_permissions table
-                    $query = "INSERT INTO roles_permissions (id_role, id_permission) VALUES (?, ?)";
+                            $query = "INSERT INTO roles_permissions (id_role, id_permission) VALUES (?, ?)";
                     $stmt = $conn->prepare($query);
                     $stmt->bind_param("ii", $roleId, $permissionId);
                     $stmt->execute();
                     $stmt->close();
                 } else {
-                    // Handle the case where the permission record doesn't exist
                     echo "Error: Permission '$permissionName' does not exist.";
                 }
             }
         }
 
-        // Redirect to a success page or display a success message
         header('Location: ../../adminEdit.php?error=success');
         exit;
     } else {
-        // Display an error message if the role name is empty
         echo 'Error: Role name cannot be empty.';
     }
 } else {
-    // Display an error message if the form has not been submitted
     echo 'Error: Form has not been submitted.';
 }
